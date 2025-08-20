d55634f967f36a51b40069779ab2d147acf13265
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-08-20 20:28:53 +0530

    pointer char array

---
+++ Pointer/pointer_char_array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    char ch [6] = "abcde";
+
+    cout<< arr << endl;
+    cout<< ch << endl;
+    //prints the entire string
+    char *p = &ch[0];
+
+    cout<< p << endl;
+
+    char temp = 'z';
+    char *c = &temp;
+
+    cout<< "->" << c << endl;
+}
