dbc3cb6bf80b722bf692b747d953fee472d195e5
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-08-25 01:26:40 +0530

    Dynamic memory allocation

---
+++ Arrays/Dynamic Memory/dynamic_array.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getSum(int arr[], int n)
+{
+    int sum = 0;
+    for(int i = 0; i<n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cin>> n;
+
+    //variable size array /dynamic array
+    int* arr = new int [n];
+
+    //taking input in array
+    for(int i = 0; i<n; i++)
+    {
+        cin>> arr[i];
+    }
+
+    int ans = getSum(arr, n);
+    cout<< "Answer is: "<< ans << endl;
+
+    //free the memory
+    delete [] arr;
+
+    return 0;
+}
