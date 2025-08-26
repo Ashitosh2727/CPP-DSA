db54f24c3745ab6365c8ff84d82e8ee30cf51c06
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-08-27 00:29:02 +0530

    Macros

---
+++ macro.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PI 3.14
+
+int main()
+{
+    // H/W : GFG Article of macros
+    int r = 5;
+    // double pi = 3.14;
+    double area = PI * r * r;
+
+    cout<< "Area of circle is: "<< area <<endl;
+
+    return 0;
+}
