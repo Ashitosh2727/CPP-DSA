78294c12e9064f77986aa8260ab9f749203a3d1d
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-09-11 20:30:44 +0530

    Multiple inheritance

---
+++ OOPs/multiple_inheritance.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Animal{
+    public:
+    int age;
+    int weight;
+
+    public:
+    void  bark()
+    {
+        cout<< "Barking" << endl;
+    }
+
+};
+
+class Human{
+    public:
+    string color;
+
+    public:
+    void speak()
+    {
+        cout<< "Speaking" << endl;
+    }
+};
+
+class Hybrid : public Animal, public Human{
+
+};
+int main()
+{
+    Hybrid obj1;
+    obj1.bark();
+    obj1.speak();
+
+    return 0;
+}
