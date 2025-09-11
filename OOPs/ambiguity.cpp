590e5c891d57d632de085a90e5ae771639c5c308
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-09-11 21:10:17 +0530

    Ambiguity.cpp

---
+++ OOPs/ambiguity.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class A{
+    public:
+    void fun()
+    {
+        cout<< "I am A" << endl;
+    }
+};
+
+class B{
+    public:
+    void fun()
+    {
+        cout<< "I am B" << endl;
+    }
+};
+
+class C : public A, public B{
+
+};
+
+
+int main()
+{
+    C obj1;
+    // obj1.fun();
+    
+    obj1.A :: fun(); 
+    obj1.B :: fun();
+}
