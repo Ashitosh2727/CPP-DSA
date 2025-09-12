ea06066543670645a42f83fc6d4a9a2e0d86d50f
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-09-12 21:20:44 +0530

    operator overloading

---
+++ OOPs/operatorOverloading.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class A{
+
+    public:
+    int a;
+    int b;
+
+    int add()
+    {
+        return a + b;
+    }
+
+    void operator+ (A &obj)
+    {
+        // int value1 = this-> a;
+        // int value2 = obj.a;
+
+        // cout<< "Output " << value2 - value1 << endl; 
+
+        cout<< "Hello World" << endl;
+    }
+
+    void operator() ()
+    {
+        cout<< "Main bracket hu "<< this->a <<endl;
+    }
+
+};
+
+int main()
+{
+    A obj1, obj2;
+
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+    obj1();
+
+    return 0;
+}
