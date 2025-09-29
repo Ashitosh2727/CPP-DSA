f28d108e99d21a2b1cb7b8945356a025586b5856
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-09-29 19:54:09 +0530

    circular linked list

---
+++ Linked List/circularLinkedList.cpp
@@ -0,0 +1,136 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+    this -> data = data;
+    this -> next = NULL;
+    }
+
+    ~Node()
+    {
+        int value = this -> data;
+        if(this -> next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+        cout<< "Memory is free of node with data "<< value << endl;
+    }
+};
+
+void insertNode(Node* &tail, int element, int data)
+{
+    //assuming that the element is present in the list
+
+    //Empty list
+    if(tail == NULL)
+    {
+        Node* newNode = new Node(data);
+        tail = newNode;
+        newNode -> next = newNode;
+    }
+    else
+    {
+        //non-empty list
+        // assuming that element is present
+        Node* curr = tail;
+
+        while (curr -> data != element)
+        {
+            curr = curr -> next;
+        }
+
+        //element found -> curr representing element wala node
+        Node* temp = new Node(data);
+        temp -> next = curr -> next;
+        curr -> next = temp;
+    }
+}
+
+void deleteNode(Node* &tail, int value)
+{
+    //empty list
+    if(tail == NULL)
+    {
+        cout<< "List is empty. please check again" <<endl;
+        return; 
+    }
+    else
+    {
+        //non - empty
+        //assuming value is present in the linked list
+        Node* prev = tail;
+        Node* curr = prev -> next;
+
+        while(curr -> data != value)
+        {
+            prev = curr;
+            curr = curr -> next;
+        }
+
+        prev -> next = curr -> next;
+        //  1 node Linked List
+        if(curr == prev)
+        {
+            tail = NULL;
+        }
+
+        // >2 Node linked list  
+        if(tail == curr)
+        {
+            tail = prev;    
+        }
+        curr -> next = NULL;
+        delete curr;
+    }
+}
+void print(Node* tail)
+{
+     Node* temp = tail;
+
+     do{
+        cout<< tail -> data << " ";
+        tail = tail -> next;
+     }while (tail != temp);  
+     cout<< endl;
+     
+}
+int main()
+{
+    Node* tail = NULL;
+
+    //empty list me insert 
+    insertNode(tail, 5, 3);
+    print(tail);
+
+    insertNode(tail, 3, 5);
+    print(tail);
+
+    insertNode(tail, 5, 7);
+    print(tail);
+
+    insertNode(tail, 7, 9);
+    print(tail);
+
+    insertNode(tail, 5, 6);
+    print(tail);
+
+    insertNode(tail, 9, 10);
+    print(tail);
+
+    insertNode(tail, 3, 4);
+    print(tail);
+
+    // deleteNode(tail, 3);
+    // print(tail);
+
+    deleteNode(tail, 3);
+    print(tail);
+    return 0;
+}
