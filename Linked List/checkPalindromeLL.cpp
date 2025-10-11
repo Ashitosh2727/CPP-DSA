1c84dd1d31cf031dc786e15551a5dae650481d39
Ashitosh2727 <ashitoshmane08@gmail.com>
2025-10-11 23:01:31 +0530

    check palindrome

--- Linked List/checkPalindromeLL.cpp
+++ Linked List/checkPalindromeLL.cpp
@@ -48,3 +48,68 @@ bool checkPalindrome(vector<int> arr)
         return checkPalindrome(arr);
     }
 };
+
+//Approach 2
+
+Node* getMid(Node* head)
+{
+    Node* slow = head;
+    Node* fast = head -> next;
+
+    while(fast != NULL && fast -> next != NULL)
+    {
+        fast = fast -> next -> next;
+        slow = slow -> next;
+    }
+    return slow;
+}
+
+Node* reverse(Node* head)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* next = NULL;
+
+    while(curr != NULL)
+    {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+bool isPalindrome(Node* head)
+{
+    if(head -> next = NULL)
+    {
+        return true;
+    }
+    
+    //step1 -> find middle
+    Node* middle = getMid(head);
+    
+    //step2 -> reverse 2nd half
+    Node* temp = middle -> next;
+    middle -> next = reverse(temp);
+
+    //step3 -> compare both halves
+    Node* head1 = head;
+    Node* head2 = middle -> next;
+
+    while(head2 !=  NULL)
+    {
+        if(head1 -> data != head2 -> data)
+        {
+            return false;
+        }
+        head1 = head -> next;
+        head2 = head2 -> next;
+    }
+
+    //step4 -> repeat step2 (optional)
+    temp = middle -> next;
+    middle -> next = reverse(temp);
+    return true;
+}
